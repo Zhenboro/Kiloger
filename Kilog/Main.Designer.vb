6720d8369077efffd9bc24e7117229d817d33f16
Zhenboro <zhenboro@outlook.com>
2022-06-12 23:07:50 -0300

    intento de interprete

---
+++ Kilog/Main.Designer.vb
@@ -0,0 +1,80 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Main
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Anchor = System.Windows.Forms.AnchorStyles.Bottom
+        Me.Button1.Location = New System.Drawing.Point(338, 516)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(109, 33)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Interpretar"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TextBox1.Location = New System.Drawing.Point(197, 490)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(390, 20)
+        Me.TextBox1.TabIndex = 1
+        Me.TextBox1.Text = "C:\Users\Zhenboro\Desktop\registroBruto.log"
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.RichTextBox1.Location = New System.Drawing.Point(12, 12)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(760, 472)
+        Me.RichTextBox1.TabIndex = 2
+        Me.RichTextBox1.Text = ""
+        '
+        'Main
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(784, 561)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Main"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Kiloger Interpreter"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents RichTextBox1 As RichTextBox
+End Class
---
+++ Kilog/Main.vb
@@ -0,0 +1,101 @@
+﻿Public Class Main
+    Dim filePath As String = Nothing
+    Private Sub Main_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Init()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Try
+            If TextBox1.Text <> Nothing Then
+                filePath = TextBox1.Text
+            End If
+            If filePath = Nothing Then
+                Dim openFile As New OpenFileDialog
+                openFile.Filter = "All file types (*.*)|*.*"
+                openFile.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop)
+                openFile.Title = "Open file..."
+                If openFile.ShowDialog() = DialogResult.OK Then
+                    filePath = openFile.FileName
+                    TextBox1.Text = filePath
+                End If
+            End If
+            Intepretar()
+        Catch ex As Exception
+            AddToLog("Button1_Click@Main", "Error: " & ex.Message, True)
+        End Try
+    End Sub
+
+    Dim registro As String
+    Dim charInicio, charFinal As Integer
+    Dim longitudRegistro As Integer
+    Dim longitudAnalisis As Integer
+    Sub Intepretar()
+        Try
+            'registro = My.Computer.FileSystem.ReadAllText(filePath)
+            'longitudRegistro = registro.Length
+            'longitudAnalisis = 0
+
+            'For Each caracter As Char In registro
+            '    Select Case caracter
+            '        Case "{"
+            '            charInicio = longitudAnalisis
+            '        Case "}"
+            '            charFinal = longitudAnalisis
+            '        Case Else
+            '            longitudAnalisis += 1
+            '            Console.WriteLine(caracter)
+            '    End Select
+            'Next
+
+            Dim contenido As String = My.Computer.FileSystem.ReadAllText(filePath)
+            contenido = contenido.Trim()
+            contenido = contenido.TrimStart()
+            contenido = contenido.TrimEnd()
+
+            contenido = contenido.Replace("{OEM1}", "´")
+            contenido = contenido.Replace("{OEM5}", "|")
+            contenido = contenido.Replace("{OEM6}", "¿")
+            contenido = contenido.Replace("{OEM7}", "{")
+            contenido = contenido.Replace("{OEMQUESTION}", "}")
+            contenido = contenido.Replace("{OEMCOMMA}", ",")
+            contenido = contenido.Replace("{OEMPERIOD}", ".")
+            contenido = contenido.Replace("{OEMMINUS}", "-")
+            contenido = contenido.Replace("{OEMTILDE}", "ñ")
+            contenido = contenido.Replace("{OEMPLUS}", "+")
+            contenido = contenido.Replace("{OEMOPENBRACKETS}", "'")
+            contenido = contenido.Replace("{OEMBACKSLASH}", "<")
+            contenido = contenido.Replace("{D1}", "1")
+            contenido = contenido.Replace("{D2}", "2")
+            contenido = contenido.Replace("{D3}", "3")
+            contenido = contenido.Replace("{D4}", "4")
+            contenido = contenido.Replace("{D5}", "5")
+            contenido = contenido.Replace("{D6}", "6")
+            contenido = contenido.Replace("{D7}", "7")
+            contenido = contenido.Replace("{D8}", "8")
+            contenido = contenido.Replace("{D9}", "9")
+            contenido = contenido.Replace("{D0}", "0")
+            contenido = contenido.Replace("{RETURN}", vbCrLf)
+            contenido = contenido.Replace("{SPACE}", " ")
+            contenido = contenido.Replace("{DIVIDE}", "/")
+            contenido = contenido.Replace("{MULTIPLY}", "*")
+            contenido = contenido.Replace("{SUBSTRACT}", "-")
+            contenido = contenido.Replace("{ADD}", "+")
+            contenido = contenido.Replace("{DECIMAL}", ".")
+            contenido = contenido.Replace("{NUMPAD0}", "0")
+            contenido = contenido.Replace("{NUMPAD1}", "1")
+            contenido = contenido.Replace("{NUMPAD2}", "2")
+            contenido = contenido.Replace("{NUMPAD3}", "3")
+            contenido = contenido.Replace("{NUMPAD4}", "4")
+            contenido = contenido.Replace("{NUMPAD5}", "5")
+            contenido = contenido.Replace("{NUMPAD6}", "6")
+            contenido = contenido.Replace("{NUMPAD7}", "7")
+            contenido = contenido.Replace("{NUMPAD8}", "8")
+            contenido = contenido.Replace("{NUMPAD9}", "9")
+
+            RichTextBox1.Text = contenido
+
+        Catch ex As Exception
+            AddToLog("Intepretar@Main", "Error: " & ex.Message, True)
+        End Try
+    End Sub
+End Class
